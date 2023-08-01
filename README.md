50e6b9b31eaeed3fcf1e2f93d051f8e222f98d84
failure64 <swastikmiddha@gmail.com>
2023-08-01 17:52:50 +0530

    fixing setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# luganodes-task
- 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Homepage from "./pages/Homepage";
-import CoinPage from "./pages/CoinPage";
 import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
 
 const useStyles = makeStyles(() => ({
   App: {
@@ -12,10 +12,13 @@ const useStyles = makeStyles(() => ({
     minHeight: "100vh",
   },
 }));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div >
+      <div className={classes.App}>
         <Header />
         <Route path="/" component={Homepage} exact />
         <Route path="/coins/:id" component={CoinPage} exact />
