c29b3aab9559b41645730064d716c5fb70e67cb8
failure64 <swastikmiddha@gmail.com>
2023-08-01 17:10:28 +0530

    test 1

--- track/src/App.js
+++ track/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import CoinPage from "./pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 function App() {
   return (
-    <div className="App">
-      Setup
-    </div>
+    <BrowserRouter>
+      <div >
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>
+    </BrowserRouter>
   );
 }
 
